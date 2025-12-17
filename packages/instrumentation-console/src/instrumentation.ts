e92f1f32e326b717d5321c0abfbd50a3d5dcedcd
DrDreo <code@drdreo.com>
2025-12-17 22:28:18 +0100

    chore: format

--- packages/instrumentation-console/src/instrumentation.ts
+++ packages/instrumentation-console/src/instrumentation.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { trace, context } from '@opentelemetry/api';
 import { SeverityNumber } from '@opentelemetry/api-logs';
 import { InstrumentationBase } from '@opentelemetry/instrumentation';
