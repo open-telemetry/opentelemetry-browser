a5cb053b4bd1f2f3a0090f7388a49cfbb291e424
Martin Kuba <martin@martinkuba.com>
2026-03-09 10:42:25 -0700

    changed to wildcard

--- packages/instrumentation/tsdown.config.ts
+++ packages/instrumentation/tsdown.config.ts
@@ -4,9 +4,7 @@ import baseConfig from '../../tsdown.config.ts';
 export default defineConfig({
   ...baseConfig,
   entry: [
-    'src/navigation-timing/index.ts',
-    'src/user-action/index.ts',
-    'src/web-vitals/index.ts',
+    'src/*/index.ts',
   ],
   dts: true,
 });
