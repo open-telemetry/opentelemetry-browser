8f435e6ed4038dab2e9e60a20b362ce84bfdd16f
overbalance <overbalance@users.noreply.github.com>
2026-03-12 16:22:28 -0500

    fix linting

--- eslint.dist.config.js
+++ eslint.dist.config.js
@@ -1,13 +1,10 @@
 import baselinePlugin from 'eslint-plugin-baseline-js';
-import baseConfig from './eslint.config.js';
 
 // Extends base config and adds compiled output checking
 export default [
-  ...baseConfig,
   // Compiled output - catches non-baseline APIs from dependencies
   {
     files: ['packages/**/dist/**/*.js'],
-    ignores: ['**/*.d.ts'],
     plugins: {
       'baseline-js': baselinePlugin,
     },
