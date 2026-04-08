fa7d9a348ce3f3bcfb26e278eb40ff58561f1418
David Luna <luna.bistuer@gmail.com>
2026-04-08 13:42:02 +0200

    chore: exclide tests files from build

--- packages/browser/tsdown.config.ts
+++ packages/browser/tsdown.config.ts
@@ -3,5 +3,5 @@ import baseConfig from '../../tsdown.config.ts';
 
 export default defineConfig({
   ...baseConfig,
-  entry: ['src/*.ts', '!src/{test-utils,utils}/index.ts'],
+  entry: ['src/*.ts', '!src/*.test.ts'],
 });
