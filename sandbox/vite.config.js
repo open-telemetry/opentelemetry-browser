c01f70b84ad931a266a267becfa00f0ef70b83fd
maxime.quentin <maxime.quentin@datadoghq.com>
2026-04-13 15:42:20 +0200

    rm vite react uneeded plugin

--- sandbox/vite.config.js
+++ sandbox/vite.config.js
@@ -1,8 +1,6 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
   // GitHub Pages serves at /opentelemetry-browser/
   base: process.env.CI ? '/opentelemetry-browser/' : '/',
 });
