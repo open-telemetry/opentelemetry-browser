adcd3b97b01a5ab0e4edc64ccc552076c2f90491
maxime.quentin <maxime.quentin@datadoghq.com>
2026-04-22 11:11:57 +0200

    Introduce new BrowserDocumentUrlInstrumentation

---
+++ packages/instrumentation/src/browser-document-url/types.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import type { InstrumentationConfig } from '@opentelemetry/instrumentation';
+
+/**
+ * BrowserDocumentUrlInstrumentation Configuration
+ */
+export interface BrowserDocumentUrlInstrumentationConfig
+  extends InstrumentationConfig {
+  // Configuration options will be added here
+}
