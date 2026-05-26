fac28ffd7d573f4ddcd9910c09ba25ddd87c1716
David Luna <luna.bistuer@gmail.com>
2026-05-26 18:18:52 +0200

    chore: add test for export configuration

--- packages/browser/src/sdk.test.ts
+++ packages/browser/src/sdk.test.ts
@@ -13,6 +13,9 @@ import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest';
 import { startBrowserSdk } from './sdk.ts';
 import type { WebSdk } from './types.ts';
 
+// Arrange
+const SCHEDULE_DELAY = 10;
+
 describe('startBrowserSdk', () => {
   const response = { ok: true, json: async () => ({ ok: true }) } as Response;
   const fetchSpy = vi.spyOn(globalThis, 'fetch').mockResolvedValue(response);
@@ -36,7 +39,7 @@ describe('startBrowserSdk', () => {
     .mockImplementation(() => tracerProvider);
   let tracerProvider: TracerProvider;
   let loggerProvider: LoggerProvider;
-  let webSdk: WebSdk;
+  let browserSdk: WebSdk;
 
   // NOTE: we mock the registration of the logger/tracer provider because
   // the APIs only allow to register once. With the mock we can use
@@ -54,12 +57,12 @@ describe('startBrowserSdk', () => {
     getLoggerProviderSpy.mockClear();
     getTracerProviderSpy.mockClear();
     fetchSpy.mockClear();
-    await webSdk?.shutdown();
+    await browserSdk?.shutdown();
   });
 
   it('should register a LoggerProvider and TracerProvider', async () => {
     // Act
-    webSdk = startBrowserSdk();
+    browserSdk = startBrowserSdk();
 
     // Assert
     expect(setGlobalLoggerProviderSpy).callCount(1);
@@ -76,11 +79,9 @@ describe('startBrowserSdk', () => {
     expect(spanProcessors[0]).toBeInstanceOf(BatchSpanProcessor);
   });
 
-  it('should use the default configuration for exporters', async () => {
-    // Arrange
-    const SCHEDULE_DELAY = 10;
+  it('should use the default configuration for batch processor', async () => {
     // Act
-    webSdk = startBrowserSdk({
+    browserSdk = startBrowserSdk({
       // NOTE: we set a short delay to speed up tests and avoid test timeouts
       processorConfig: {
         scheduledDelayMillis: SCHEDULE_DELAY,
@@ -102,5 +103,34 @@ describe('startBrowserSdk', () => {
         (args) => args[0] === 'http://localhost:4318/v1/traces',
       ),
     ).toBeDefined();
+  });
+
+  it('should accept exporter confgiration with URL and headers', async () => {
+    // Act
+    browserSdk = startBrowserSdk({
+      processorConfig: {
+        // NOTE: we set a short delay to speed up tests and avoid test timeouts
+        scheduledDelayMillis: SCHEDULE_DELAY,
+      },
+      exportConfig: {
+        url: 'http://otlp-signal-endpoint:4318',
+        headers: { bar: 'baz' },
+      },
+    });
+    logs.getLogger('logs-sdk-test').emit({ eventName: 'test' });
+    trace.getTracer('traces-sdk-test').startSpan('test').end();
+    await new Promise((r) => setTimeout(r, SCHEDULE_DELAY + 5));
+
+    // Assert
+    expect(fetchSpy).toHaveBeenCalledTimes(2);
+    fetchSpy.mock.calls.forEach((args) => {
+      expect(args[1]).containSubset({
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          bar: 'baz',
+        },
+      });
+    });
   });
 });
