fa2b3fcfc5076ae6441e25cc87c7dbd7581d1ccb
David Luna <luna.bistuer@gmail.com>
2026-06-12 17:23:07 +0200

    chore: add index file

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { getDefaultContextManager } from './context.ts';
+export { startLogsSdk } from './logs.ts';
+export { getDefaultPropagators } from './propagation.ts';
+export { getDefaultSampler } from './sampler.ts';
+export { quickStartBrowserSdk, startBrowserSdk } from './start.ts';
+export { startTracesSdk } from './traces.ts';
