2f0293913b3a5432ddd46e27eb4c4e351ec9f0a5
maxime.quentin <maxime.quentin@datadoghq.com>
2026-06-18 10:56:34 +0200

    introduce @opentelemetry/browser-sdk/session

---
+++ packages/sdk/src/session/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { SessionLogRecordProcessor } from './SessionLogRecordProcessor.ts';
+export { SessionSpanProcessor } from './SessionSpanProcessor.ts';
+export type { SessionProvider } from './types/SessionProvider.ts';
