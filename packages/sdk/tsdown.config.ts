32f7ccc2e313a5aa67ea7089d9427c678a1554cb
overbalance <overbalance@users.noreply.github.com>
2026-07-02 10:58:21 -0700

    refactor(sdk): rename sdk folder to initializer

--- packages/sdk/tsdown.config.ts
+++ packages/sdk/tsdown.config.ts
@@ -4,9 +4,9 @@ import baseConfig from '../../tsdown.config.ts';
 export default defineConfig({
   ...baseConfig,
   entry: {
-    logs: 'src/sdk/logs.ts',
-    traces: 'src/sdk/traces.ts',
-    start: 'src/sdk/start.ts',
+    logs: 'src/initializer/logs.ts',
+    traces: 'src/initializer/traces.ts',
+    start: 'src/initializer/start.ts',
     'session/index': 'src/session/index.ts',
   },
 });
