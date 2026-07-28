8f81077113a94b7272c49be6918fb8a9cca98733
David Luna <luna.bistuer@gmail.com>
2026-07-28 14:14:58 +0200

    chore: tune validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -226,11 +226,9 @@ function checkPackageExports(units) {
 }
 
 // NOTE: some packages may grow bigger than the instrumentations like
-// - sdk: includes the required components to register the required providers
-// - utils: code shared by the instrumentations
+// - instrumentation/utils: code shared by the instrumentations
 const MAX_SIZE_KB_BUCKETS = {
-  sdk: 8,
-  utils: 5,
+  'instrumentation/utils': 5,
 };
 function checkBundleSize(units) {
   logSection('4. Bundle Size');
@@ -238,7 +236,6 @@ function checkBundleSize(units) {
   let allPassed = true;
 
   for (const { label, distPath } of units) {
-    // NOTE: the SDK package is expected to grow bigger than the instrumentations
     const MAX_SIZE_KB = MAX_SIZE_KB_BUCKETS[label] || 4;
     const jsFilePaths = getJsFiles(distPath);
 
