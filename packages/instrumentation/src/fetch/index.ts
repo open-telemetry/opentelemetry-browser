2b29db538d79ae520c8c1cf258bd2ade0a9f52e5
David Luna <luna.bistuer@gmail.com>
2026-07-28 13:52:00 +0200

    chore: add entrypoint and docs

---
+++ packages/instrumentation/src/fetch/index.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { FetchInstrumentation } from './instrumentation.ts';
+export type {
+  FetchCustomAttributeFunction,
+  FetchRequestHookFunction,
+  FetchError,
+  FetchResponse,
+  FetchInstrumentationConfig,
+} from './types.ts';
