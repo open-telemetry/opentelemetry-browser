257568dedb2e9c6293e280561b0d9e63ce83a555
Maxime Quentin <maxime.quentin@datadoghq.com>
2026-09-07 16:01:30 +0200

    feat(sdk): add LocationDocumentProvider
    Signed-off-by: Maxime Quentin <maxime.quentin@datadoghq.com>

--- packages/sdk/src/document/index.ts
+++ packages/sdk/src/document/index.ts
@@ -5,9 +5,12 @@
 
 export { DocumentLogRecordProcessor } from './DocumentLogRecordProcessor.ts';
 export { DocumentSpanProcessor } from './DocumentSpanProcessor.ts';
+export type { LocationDocumentProviderConfig } from './LocationDocumentProvider.ts';
+export { LocationDocumentProvider } from './LocationDocumentProvider.ts';
 export { ATTR_BROWSER_DOCUMENT_URL_FULL } from './semconv.ts';
 export type { DocumentProvider } from './types/DocumentProvider.ts';
 export {
   createDocumentLogRecordProcessor,
   createDocumentSpanProcessor,
+  createLocationDocumentProvider,
 } from './utils.ts';
--- packages/sdk/src/document/utils.ts
+++ packages/sdk/src/document/utils.ts
@@ -7,6 +7,8 @@ import type { LogRecordProcessor } from '@opentelemetry/sdk-logs';
 import type { SpanProcessor } from '@opentelemetry/sdk-trace';
 import { DocumentLogRecordProcessor } from './DocumentLogRecordProcessor.ts';
 import { DocumentSpanProcessor } from './DocumentSpanProcessor.ts';
+import type { LocationDocumentProviderConfig } from './LocationDocumentProvider.ts';
+import { LocationDocumentProvider } from './LocationDocumentProvider.ts';
 import type { DocumentProvider } from './types/DocumentProvider.ts';
 
 export function createDocumentSpanProcessor(
@@ -19,4 +21,10 @@ export function createDocumentLogRecordProcessor(
   documentProvider: DocumentProvider,
 ): LogRecordProcessor {
   return new DocumentLogRecordProcessor(documentProvider);
+}
+
+export function createLocationDocumentProvider(
+  config?: LocationDocumentProviderConfig,
+): DocumentProvider {
+  return new LocationDocumentProvider(config);
 }
